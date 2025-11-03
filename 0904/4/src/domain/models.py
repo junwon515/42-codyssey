18f4496e787fd5c3e511dcafdb5c7d74abc27886
Junwon515 <wnsdnjs7731@gmail.com>
2025-11-03 19:34:28 +0900

    fix: Domain error solved

--- 0904/4/src/domain/models.py
+++ 0904/4/src/domain/models.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Todo:
-    id: str = field(default_factory=lambda: str(uuid.uuid4()))
     task: str
     due_date: str | None = None
+    id: str = field(default_factory=lambda: str(uuid.uuid4()))
     is_completed: bool = False
 
     def complete(self) -> None:
